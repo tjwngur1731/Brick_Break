a7f9d51494b08df4e7230b79262e36a1a4d2a3a3
martin-newbie <g.martin.min@gmail.com>
2020-11-17 09:04:08 +0900

    MAIN

---
+++ Assets/Scripts/SkinScripts/MoneyText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyText : MonoBehaviour
+{
+    private Text myMoney;
+    void Start()
+    {
+        myMoney = GameObject.Find("Money").GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        SetMoney();
+    }
+
+    void SetMoney()
+    {
+        myMoney.text = ShopManager.userMoney.ToString();
+    }
+}
---
+++ Assets/Scripts/SkinScripts/SkinChoose.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinChoose : MonoBehaviour
+{
+    public GameObject[] obj;
+    public bool skinAvailble = false;
+    static int currentSkin;
+
+    private void Awake()
+    {
+        currentSkin = PlayerPrefs.GetInt("Skin");
+        Debug.Log(currentSkin + " awake");
+
+        //if (skinAvailble)
+        {
+            obj[currentSkin].SetActive(true);
+        }
+    }
+
+    public void SkinAvailble()
+    {
+
+
+
+        for (int i = 0; i < obj.Length; i++)
+        {
+            Debug.Log(ShopManager.instance.buySkin[i]);
+
+            if (ShopManager.instance.buySkin[i])
+            {
+                for (int j = 0; j < obj.Length; j++)
+                {
+                    obj[j].SetActive(false);
+                }
+                obj[i].SetActive(true);
+                PlayerPrefs.SetInt("Skin",i);
+                Debug.Log(i + " setInt");
+                skinAvailble = true;
+                Debug.Log(i);
+            }
+            
+        }
+    }
+}
