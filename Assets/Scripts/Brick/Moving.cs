9a674db51d16aee01635b4f3faf8ff5a9324c7b6
tjwngur1731 <1sjhjs1@gmail.com>
2020-11-19 22:12:50 +0900

    !최종!

--- Assets/Scripts/Brick/Moving.cs
+++ Assets/Scripts/Brick/Moving.cs
@@ -5,15 +5,28 @@ public class Moving : MonoBehaviour
     bool move;
     int _x2, _y2;
 
-    void Update() { if (move) Move(_x2, _y2); }
+    private void Start()
+    {
+    }
+
+    void Update()
+    {
+
+        transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(0.65f, 0.65f, 1), 0.3f);
+
+        if (move) Move(_x2, _y2);
+
+
+    }
+
 
     public void Move(int x2, int y2)
-    {
-        move = true; _x2 = x2; _y2 = y2;
-        transform.position = Vector3.MoveTowards(transform.position, new Vector3(0.75f * x2 - 2.25f, -0.75f * y2 + 1.5f, 0), 0.25f);
-        if (transform.position == new Vector3(0.75f * x2 - 2.25f, -0.75f * y2 + 1.5f, 0))
         {
-            move = false;
+            move = true; _x2 = x2; _y2 = y2;
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(0.75f * x2 - 2.25f, -0.75f * y2 + 1.5f, 0), 0.1f);
+            if (transform.position == new Vector3(0.75f * x2 - 2.25f, -0.75f * y2 + 1.5f, 0))
+            {
+                move = false;
+            }
         }
     }
-}
